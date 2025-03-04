2231eacc14db7a68a02148e7b42580ab08fca347
kyssiei <kyssiei.kay@gmail.com>
2025-03-04 00:24:08 -0500

    updated code

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,7 +5,13 @@ function Content() {
     
     return(
         <>
-        
+            <h2>My Top 3 Favorite Foods</h2>
+            <ul>
+                <li>Ethiopian Food</li>
+                <li>Hatian Food</li>
+                <li>Indian Food</li>
+            </ul>
+            
         </>
     )
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,8 @@ function Header() {
     
     return(
         <>
-        
+         <h1>Hello World im Kayla </h1>
+         <h2>Here's a little bit about me..</h2>
         </>
     )
 
